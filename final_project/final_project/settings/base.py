beae75bd7e7ffae5f5c7bff304a60abd307caf15
umjiwoo <uts417923@gmail.com>
2024-03-16 20:55:52 +0900

    CI/CD test

--- final_project/final_project/settings/base.py
+++ final_project/final_project/settings/base.py
@@ -14,9 +14,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
--- final_project/manage.py
+++ final_project/manage.py
@@ -8,7 +8,7 @@ import dotenv
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_project.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_project.settings.prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
